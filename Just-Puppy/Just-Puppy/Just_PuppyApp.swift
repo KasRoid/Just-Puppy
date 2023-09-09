27b3871832985cb781a94779956b45488ef61475
Doyoung Song <sdy@Lukas-MBA.kornet>
2023-09-09 16:05:48 +0900

    init: Setup Project

---
+++ Just-Puppy/Just-Puppy/Just_PuppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_PuppyApp.swift
+//  Just-Puppy
+//
+//  Created by Doyoung Song on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Just_PuppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
